00441fc8def33604c8c58f04ca3c8e666feffbd7
AncientG7eek <167808115+AncientG7eek@users.noreply.github.com>
2025-05-20 17:19:48 +0200

    fork from biopython

---
+++ Bio/Align/AlignInfo.py
@@ -0,0 +1,34 @@
+# Copyright 2000 Brad Chapman.  All rights reserved.
+#
+# This file is part of the Biopython distribution and governed by your
+# choice of the "Biopython License Agreement" or the "BSD 3-Clause License".
+# Please see the LICENSE file that should have been included as part of this
+# package.
+"""Extract information from alignment objects.
+
+In order to try and avoid huge alignment objects with tons of functions,
+functions which return summary type information about alignments should
+be put into classes in this module.
+"""
+
+
+class SummaryInfo:
+    """Calculate summary info about the alignment.
+
+    This class should be used to calculate information summarizing the
+    results of an alignment. This may either be straight consensus info
+    or more complicated things.
+    """
+
+    def __init__(self, alignment):
+        """Initialize with the alignment to calculate information on.
+
+        ic_vector attribute. A list of ic content for each column number.
+        """
+        self.alignment = alignment
+        self.ic_vector = []
+
+    def get_column(self, col):
+        """Return column of alignment."""
+        # TODO - Deprecate this and implement slicing?
+        return self.alignment[:, col]
